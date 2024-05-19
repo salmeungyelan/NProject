dcbaac739a0bb8ebb528d0a5757e8a70fdfba650
sunset <codinz.n@gmail.com>
2024-05-20 04:14:23 +0900

    Design: 공통으로 쓰일 디자인 추가
    리셋 디자인 및 공통으로 쓰일 디자인, 색상 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,38 +1,45 @@
 const PALETTE = {
-	mainColor: {
-		orange: '#EA5400',
-		navy: '#003E53',
-		beige: '#F7F1E8',
-		gray: '#666666',
-		black: '#111111',
+	red: {
+		100: '#CC0000',
+		200: '#FDF4F4',
 	},
 
-	fontColor: '#FFFFFF',
-	hover: '#F6F6F6',
+	orange: {
+		100: '#DE5000',
+		200: '#EA5400',
+	},
+
+	gray: {
+		100: '#999',
+		200: '#666',
+		300: '#333',
+	},
+
+	white: {
+		100: '#FFFFFF',
+		200: '#E0E0E0',
+	},
+
+	navy: '#003E53',
+	ivory: '#F7F1E8',
+	black: '#111111',
 };
 
 const FONT_SIZE = {
 	es: '0.625rem', // 10
 	xs: '0.75rem', // 12
 	sm: '0.875rem', // 14
-	md: '1rem', // 16
+	re: '1rem', // 16
+	md: '1.125rem', // 18
 	big: '1.25rem', // 20
+	mb: '1.375rem', // 22
 	lg: '1.5rem', // 24
-	xl: '2.5rem',
-};
-
-const FONT_WEIGHT = {
-	extraLight: 'Pretendard-ExtraLight', // 200
-	light: 'Pretendard-Light', // 300
-	regular: 'Pretendard-Regular', // 400
-	medium: 'Pretendard-Medium', // 500
-	bold: 'Pretendard-Bold', // 700
+	xl: '2.25rem', // 36
 };
 
 const theme = {
 	PALETTE,
 	FONT_SIZE,
-	FONT_WEIGHT,
 };
 
 export default theme;
