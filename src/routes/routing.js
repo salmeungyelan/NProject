65cdd7d0354b0a1ded47b3a55fee1beab77cca8c
sunset <codinz.n@gmail.com>
2024-05-20 04:37:49 +0900

    Feat: routing 설정
    routing 설정(각 페이지 경로 설정)

--- src/routes/routing.js
+++ src/routes/routing.js
@@ -1,19 +1,39 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import Layout from '../components/@common/Layout/index';
-import Home from '../pages/Home/index';
+import Layout from 'components/@common/Layout';
+import Home from 'pages/Home';
+import FindId from 'pages/FindId';
+import FindPw from 'pages/FindPw';
+import Register from 'pages/Register';
+import Main from 'pages/Main';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-    ],
-  },
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '',
+				element: <Home />,
+			},
+			{
+				path: '/find/id',
+				element: <FindId />,
+			},
+			{
+				path: '/find/password',
+				element: <FindPw />,
+			},
+			{
+				path: '/register',
+				element: <Register />,
+			},
+			{
+				path: '/main',
+				element: <Main />,
+			},
+		],
+	},
 ]);
 
 export default router;
