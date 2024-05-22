4e5b62be1f5b5b198a9f7d82105cc00d525b46a5
sunset <codinz.n@gmail.com>
2024-05-22 23:57:26 +0900

    Style: import 코드 순서 변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import { RecoilRoot } from 'recoil';
 import { RouterProvider } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
 import GlobalStyles from 'styles/global';
+
 import router from 'routes/routing';
+import Loading from 'components/@common/Loading';
+import ErrorFallback from 'components/@common/Error';
 
 function App() {
-  return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </RecoilRoot>
-  );
+	return (
+		<ErrorBoundary FallbackComponent={ErrorFallback}>
+			<Suspense fallback={<Loading />}>
+				<RecoilRoot>
+					<ThemeProvider theme={theme}>
+						<GlobalStyles />
+						<RouterProvider router={router} />
+					</ThemeProvider>
+				</RecoilRoot>
+			</Suspense>
+		</ErrorBoundary>
+	);
 }
 
 export default App;
