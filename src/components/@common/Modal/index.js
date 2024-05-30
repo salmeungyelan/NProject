e5add6ce7e8121c1d788c96aa4a3450051af4131
sunset <codinz.n@gmail.com>
2024-05-30 20:37:35 +0900

    Design: 공통으로 쓰일 버튼 및 그외 디자인 추가
    공통으로 사용되는 버튼 / 모달 등 그 외 추가

---
+++ src/components/@common/Modal/index.js
@@ -0,0 +1,7 @@
+import * as S from './index.styles';
+
+function Modal() {
+	return <S.Modal>modal</S.Modal>;
+}
+
+export default Modal;
---
+++ src/components/@common/Notice/index.js
@@ -0,0 +1,12 @@
+import * as S from './index.styles';
+
+function Notice() {
+	return (
+		<S.Notice>
+			<img src="/assets/images/icons/register-x-btn.svg" alt="" />
+			<p>올바르지 않은 계정 정보입니다.</p>
+		</S.Notice>
+	);
+}
+
+export default Notice;
