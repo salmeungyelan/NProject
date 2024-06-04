e122912c17bd0e0e364fb11757c03606d92e4597
sunset <codinz.n@gmail.com>
2024-06-05 00:12:16 +0900

    Design: 공통으로 사용될 모달 퍼블리싱
    알림용으로 사용될 모달 디자인 완료되어 퍼블리싱

--- src/components/@common/Modal/index.styles.js
+++ src/components/@common/Modal/index.styles.js
@@ -1,6 +1,52 @@
 import styled from 'styled-components';
-import { ModalBackground } from 'styles/common';
+import { ModalBackground, flexColumnCenter } from 'styles/common';
 
-export const Modal = styled.div`
+export const Background = styled.div`
 	${ModalBackground}
+	z-index: 101;
+`;
+
+export const Container = styled.div`
+	position: fixed;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	background-color: ${({ theme }) => theme.PALETTE.white[100]};
+	max-width: 400px;
+	width: 80%;
+	padding: 32px 30px;
+	${flexColumnCenter}
+	gap: 36px;
+	transition: all ease 0.3s;
+
+	@media screen and (max-width: 768px) {
+		width: 294px;
+		transition: all ease 0.3s;
+	}
+`;
+
+export const Header = styled.div`
+	${flexColumnCenter}
+	gap: 22px;
+
+	> img {
+		width: 40px;
+	}
+`;
+
+export const Title = styled.p`
+	color: ${({ theme }) => theme.PALETTE.gray[300]};
+	font-size: ${({ theme }) => theme.FONT_SIZE.md};
+	font-weight: 500;
+`;
+
+export const Body = styled.div`
+	font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+	font-weight: 400;
+	color: ${({ theme }) => theme.PALETTE.gray[200]};
+`;
+
+export const ButtonBox = styled.div`
+	width: 85%;
+	cursor: pointer;
 `;
