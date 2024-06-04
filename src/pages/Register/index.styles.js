a66c95d36ebbec51dd20ce7b0ad466239a4b9ed1
sunset <codinz.n@gmail.com>
2024-06-05 00:28:33 +0900

    Design: 회원가입 페이지 퍼블 수정
    디자인 변경으로 회원가입 페이지 퍼블리싱 수정

--- src/pages/Register/index.styles.js
+++ src/pages/Register/index.styles.js
@@ -20,17 +20,48 @@ export const LogoBox = styled.div`
 export const FormBox = styled.form`
 	width: 320px;
 	${flexColumnCenter}
-	gap: 12px;
+	gap: 6px;
 `;
 
 export const InputBox = styled.div`
 	width: 320px;
 
 	> p {
+		height: 16px;
 		margin-top: 4px;
 		font-weight: 400;
 		font-size: ${({ theme }) => theme.FONT_SIZE.xs};
 		color: ${({ theme }) => theme.PALETTE.red[100]};
+	}
+`;
+
+export const EX = styled.div`
+	${flexAlignCenter}
+	margin-top: 4px;
+
+	> span {
+		font-weight: 400;
+		font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+		color: ${({ theme }) => theme.PALETTE.gray[100]};
+	}
+
+	> img {
+		width: 21px;
+	}
+`;
+
+export const Address = styled(InputBox)`
+	${flexColumn}
+	gap: 6px;
+
+	> div {
+		${flexAlignCenter}
+		gap: 6px;
+
+		> button {
+			font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+			font-weight: 400;
+		}
 	}
 `;
 
@@ -45,8 +76,7 @@ export const CheckBox = styled.div`
 	width: 100%;
 
 	> div {
-		height: 120px;
-		border: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
+		border: 1px solid ${({ theme }) => theme.PALETTE.gray[0]};
 		border-radius: 8px;
 		padding: 14px 22px;
 	}
@@ -65,14 +95,43 @@ export const Check = styled.div`
 	gap: 13px;
 `;
 
-export const CheckItem = styled.div`
-	${flexAlignCenter}
+export const CheckAll = styled.div`
+	display: flex;
 	gap: 8px;
 	font-weight: 400;
 
 	> h1 {
 		color: ${({ theme }) => theme.PALETTE.orange[100]};
 	}
+
+	> input {
+		position: absolute;
+		clip: rect(0 0 0 0);
+		width: 1px;
+		height: 1px;
+		overflow: hidden;
+	}
+
+	> label::before {
+		display: inline-block;
+		content: '';
+		width: 16px;
+		height: 16px;
+		border-radius: 2px;
+		border: 1px solid ${({ theme }) => theme.PALETTE.gray[0]};
+		background-color: ${({ theme }) => theme.PALETTE.white[100]};
+	}
+
+	> input:checked + label::before {
+		width: 18px;
+		height: 18px;
+		background-image: url('/assets/icons/register-check.svg');
+		border: none;
+	}
+`;
+
+export const CheckItem = styled(CheckAll)`
+	${flexAlignCenter}
 
 	> p {
 		cursor: pointer;
@@ -84,13 +143,9 @@ export const CheckItem = styled.div`
 		font-size: ${({ theme }) => theme.FONT_SIZE.xs};
 		color: ${({ theme }) => theme.PALETTE.orange[100]};
 	}
-
-	> input {
-		width: 16px;
-		height: 16px;
-	}
 `;
 
 export const ButtonBox = styled.div`
-	margin-top: 16px;
+	width: 100%;
+	margin-top: 24px;
 `;
