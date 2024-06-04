59ac8abdf4b80b99f0e6e2ad2e4981f998d7d54d
sunset <codinz.n@gmail.com>
2024-06-05 00:20:38 +0900

    Design: 아이디 찾기 페이지 퍼블리싱
    디자인 변경으로 인해 수정

--- src/pages/FindId/index.js
+++ src/pages/FindId/index.js
@@ -5,78 +5,29 @@ import useLayout from 'hooks/useLayout';
 import * as S from './index.styles';
 
 import Logo from 'components/@common/Logo';
-import Input from 'components/@common/Input';
-import Notice from 'components/@common/Notice';
-import Button from 'components/@common/Button';
+import IdLookup from 'components/pages/FindId/IdLookup';
+import IdResult from 'components/pages/FindId/IdResult';
 
 function FindId() {
 	useLayout();
 
 	return (
-		<>
-			<S.Body>
-				<S.Top>
-					<S.LogoBox>
-						<Link to="/">
-							<Logo size={'default'} />
-						</Link>
-					</S.LogoBox>
+		<S.Body>
+			<S.Top>
+				<S.LogoBox>
+					<Link to="/">
+						<Logo size={'default'} />
+					</Link>
+				</S.LogoBox>
 
-					<S.FormBox action="">
-						<S.InputBox>
-							<S.Title>업체명</S.Title>
-							<Input
-								size={'default'}
-								type="text"
-								autoComplete="off"
-								placeholder="업체명을 입력해 주세요."
-								required
-							/>
-						</S.InputBox>
+				<IdResult />
+				{/* <IdLookup /> */}
+			</S.Top>
 
-						<S.InputBox>
-							<S.Title>전화번호</S.Title>
-							<Input
-								size={'default'}
-								type="text"
-								autoComplete="off"
-								placeholder="전화번호를 입력해 주세요."
-								required
-							/>
-						</S.InputBox>
-
-						<S.InputBox>
-							<S.Title>사업자 등록 번호</S.Title>
-							<Input
-								size={'default'}
-								type="text"
-								autoComplete="off"
-								placeholder="사업자 등록 번호를 입력해 주세요."
-								required
-							/>
-						</S.InputBox>
-
-						<S.ButtonBox>
-
-							<Notice />
-
-							<Button
-								shape={'default'}
-								variant={'default'}
-								size={'default'}
-								type="submit"
-							>
-								아이디 찾기
-							</Button>
-						</S.ButtonBox>
-					</S.FormBox>
-				</S.Top>
-
-				<S.Bottom>
-					<img src="/assets/images/id-bottom-img.png" />
-				</S.Bottom>
-			</S.Body>
-		</>
+			<S.Bottom>
+				<img src="/assets/images/id-bottom-img.png" />
+			</S.Bottom>
+		</S.Body>
 	);
 }
 
--- src/pages/FindId/index.styles.js
+++ src/pages/FindId/index.styles.js
@@ -18,34 +18,6 @@ export const LogoBox = styled.div`
 	${flexCenter}
 `;
 
-export const FormBox = styled.form`
-	width: 320px;
-	${flexColumn}
-	gap: 20px;
-
-	> div:nth-child(3) {
-		margin-bottom: -20px;
-	}
-`;
-
-export const InputBox = styled.div`
-	width: 100%;
-	${flexColumn}
-	gap: 16px;
-`;
-
-export const Title = styled.h1`
-	font-weight: 400;
-	font-size: ${({ theme }) => theme.FONT_SIZE.re};
-	color: ${({ theme }) => theme.PALETTE.orange[100]};
-`;
-
-export const ButtonBox = styled.div`
-	margin-top: 8px;
-	${flexColumnCenter}
-	gap: 20px;
-`;
-
 export const Bottom = styled.div`
 	width: 100%;
 
