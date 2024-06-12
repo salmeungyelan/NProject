965fe90f9a92ded6d4755793bf3bd28090be8bb3
sunset <codinz.n@gmail.com>
2024-06-13 04:59:04 +0900

    Design: 공통 컴포넌트 textarea 추가

--- src/components/@common/Textarea/index.styles.js
+++ src/components/@common/Textarea/index.styles.js
@@ -9,45 +9,30 @@ const variantCSS = {
 			border: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
 		}
 	`,
-	login: css`
-		color: ${({ theme }) => theme.PALETTE.gray[200]};
-		border: 1px solid ${({ theme }) => theme.PALETTE.gray[0]};
-
-		&:focus {
-			border: 1px solid ${({ theme }) => theme.PALETTE.orange[100]};
-		}
-	`,
 };
 
 const sizeCSS = {
 	default: css`
 		width: 100%;
-		height: 40px;
-	`,
-	height: css`
-		width: 100%;
-		height: 28px;
+		height: 96px;
 
 		@media screen and (min-width: 768px) {
-			height: 32px;
-		}
-
-		@media screen and (min-width: 1200px) {
-			height: 40px;
+			height: 146px;
 		}
 	`,
 };
 
-export const Input = styled.input`
+export const Textarea = styled.textarea`
 	${({ size }) => sizeCSS[size]};
 	${({ $variant }) => variantCSS[$variant]}
 
 	font-size: ${({ theme }) => theme.FONT_SIZE.ms};
 	font-weight: 400;
-
-	padding: 0px 10px;
+	line-height: 1.5;
+	white-space: pre-wrap;
+	padding: 10px;
 	border-radius: 4px;
-
+	resize: none;
 	outline: none;
 
 	&::placeholder {
