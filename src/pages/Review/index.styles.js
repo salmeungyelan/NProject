a778e03c0a89f1e01e3abd56a608f3a839c4ee8f
sunset <codinz.n@gmail.com>
2024-06-12 18:50:27 +0900

    Design: 리뷰리스트 페이지 디자인완성

---
+++ src/pages/Review/index.styles.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import {
+	bodyContainer,
+	flexColumn,
+	flexSpaceBetweenCenter,
+} from 'styles/common';
+
+export const Body = styled.div`
+	${bodyContainer}
+	${flexColumn}
+  gap: 32px;
+`;
+
+export const MainBox = styled.div`
+	${flexColumn}
+	gap: 18px;
+
+	@media screen and (min-width: 768px) {
+		gap: 32px;
+	}
+`;
+
+export const Main = styled.div`
+	${flexColumn}
+	gap: 12px;
+
+	@media screen and (min-width: 768px) {
+		gap: 18px;
+	}
+`;
+
+export const SelectBox = styled.div`
+	${flexSpaceBetweenCenter}
+	align-items: flex-start;
+	gap: 6px;
+`;
+
+export const CardBox = styled.div`
+	display: grid;
+	grid-gap: 12px 8px;
+	grid-template-columns: repeat(2, 1fr);
+
+	@media screen and (min-width: 768px) {
+		grid-gap: 13px 11px;
+		grid-template-columns: repeat(3, 1fr);
+	}
+
+	@media screen and (min-width: 1200px) {
+		grid-gap: 16px 15px;
+		grid-template-columns: repeat(5, 1fr);
+	}
+`;
