dcbeb859d7e6798f548d42ac7261cae1be3083de
sunset <codinz.n@gmail.com>
2024-06-17 05:23:26 +0900

    Design: 아이디 및 비밀번호 찾기 본문 퍼블리싱 수정

---
+++ src/components/pages/FindAccounts/AccountLookup/index.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { flexColumn, flexColumnCenter } from 'styles/common';
+
+export const FormBox = styled.form`
+	width: 320px;
+	${flexColumn}
+	gap: 6px;
+
+	> div:nth-child(3) {
+		margin-bottom: -20px;
+	}
+`;
+
+export const ButtonBox = styled.div`
+	margin-top: 30px;
+	${flexColumnCenter}
+	gap: 16px;
+`;
