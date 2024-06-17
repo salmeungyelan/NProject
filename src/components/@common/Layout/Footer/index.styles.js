d176746e4ac33cab024521e8d9e139ec9127d1a9
sunset <codinz.n@gmail.com>
2024-06-18 04:21:37 +0900

    Design: footer css 수정

--- src/components/@common/Layout/Footer/index.styles.js
+++ src/components/@common/Layout/Footer/index.styles.js
@@ -4,6 +4,7 @@ import { flexCenter } from 'styles/common';
 export const Footer = styled.div`
 	width: 100%;
 	${flexCenter}
+	margin-bottom: 66px;
 
 	height: 100px;
 	background-color: ${({ theme }) => theme.PALETTE.gray[300]};
@@ -11,4 +12,8 @@ export const Footer = styled.div`
 	color: ${({ theme }) => theme.PALETTE.white[200]};
 	font-size: ${({ theme }) => theme.FONT_SIZE.ms};
 	font-weight: 300;
+
+	@media screen and (min-width: 768px) {
+		margin: 0;
+	}
 `;
