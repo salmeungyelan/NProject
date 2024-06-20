55a9c49e67a4d7dced8805fca41172423f61532b
sunset <codinz.n@gmail.com>
2024-06-21 05:40:33 +0900

    Fix: link.js 수정
    메인 화면과 로그인 화면 route 수정

--- src/constants/link.js
+++ src/constants/link.js
@@ -1,15 +1,26 @@
 const LINK = {
-	HOME: '/main',
-	MY: '/my_page',
+	HOME: '/',
+
+	LOGIN: '/login',
+	REGISTER: '/register',
+	FIND_ID: '/find/id',
+	FIND_PW: '/find/password',
+
 	REVIEW: '/review',
+	REVIEW_APPLY: '/review/apply',
 	REVIEW_POST: '/review/post',
+
 	NOTICE: '/notice',
 	NOTICE_POST: '/notice/post',
+
 	GUIDE: '/user_guide',
 	GUIDE_POST: '/user_guide/post',
+
+	TEAM: '/review_team',
 	VIEW: '/view_instagram',
 	DEVELOP: '/web_creation',
-	TEAM: '/review_team',
+
+	MY: '/my_page',
 };
 
 export default LINK;
