a2dce1246ce77eee4426530df8df0b8f99597668
sunset <codinz.n@gmail.com>
2024-06-22 01:55:23 +0900

    Feat: 비밀번호 변경 로직 추가
    추후 로직 수정

---
+++ src/components/pages/MyPage/PwdModal/index.js
@@ -0,0 +1,149 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import useInput from 'hooks/useInput';
+import useApi from 'hooks/useApi';
+import MESSAGE from 'constants/message';
+import useModal from 'hooks/useModal';
+import LINK from 'constants/link';
+
+import * as S from './index.styles';
+
+import Button from 'components/@common/Button';
+import Input from 'components/@common/Input';
+import Modal from 'components/@common/Modal';
+
+function PwdModal({ onClose, userId }) {
+	const navigate = useNavigate();
+
+	const [match, setMatch] = useState('');
+
+	const { handleChange, inputData } = useInput();
+	const { modalState, closeModal, openModal } = useModal();
+
+	const { password, newPassword, newPwCheck } = inputData;
+
+	const { trigger: matchTrigger } = useApi({
+		path: `/users/password-match/${userId}`,
+		shouldFetch: false,
+	});
+
+	const { trigger: updateTrigger } = useApi({
+		path: `/users/update-password/${userId}`,
+		shouldFetch: false,
+	});
+
+	// 1. 현재 비밀번호가 입력됐는지 확인
+	// 2. 현재 비밀번호가 맞는지 확인 api -> match
+	// 3. 맞다면 새 비밀번호가 입력됐는지 확인 / 틀리다면 메세지
+	// 4. 새 비밀번호 확인이 입력됐는지 확인
+	// 5. 새 비밀번호와 새 비밀번호 확인이 일치하는지 확인
+	// 6. 비밀번호 업데이트 api -> update
+	// 7. 비밀번호 변경됐다는 openModal
+	// 8. 모달 닫으면 다시 마이페이지로 이동
+
+	const handleChangePw = async () => {
+		// 1. 현재 비밀번호가 입력됐는지 확인
+		if (password) {
+			// 2. 현재 비밀번호가 맞는지 확인 api -> match
+			const req = await matchTrigger({
+				method: 'post',
+				data: { password },
+			});
+
+			if (req.data) {
+				// 3. 맞다면 새 비밀번호가 입력됐는지 확인 / 틀리다면 메세지
+				if (newPassword) {
+					// 4. 새 비밀번호 확인이 입력됐는지 확인
+					if (newPwCheck) {
+						// 5. 새 비밀번호와 새 비밀번호 확인이 일치하는지 확인
+						if (newPassword === newPwCheck) {
+							const data = {
+								password,
+								newPassword,
+							};
+
+							// 6. 비밀번호 업데이트 api -> update
+							const request = await updateTrigger({
+								method: 'patch',
+								data,
+							});
+
+							// 7. 비밀번호 변경됐다는 openModal
+							if (request.data) return openModal();
+						} else setMatch(MESSAGE.PASSWORD.CHECK);
+					} else setMatch(MESSAGE.PASSWORD.NEW_CHECK);
+				} else setMatch(MESSAGE.PASSWORD.NEW);
+			} else setMatch(MESSAGE.PASSWORD.FAIL);
+		}
+	};
+
+	// 8. 모달 닫으면 다시 마이페이지로 이동
+	const handleCloseModal = () => {
+		closeModal();
+		navigate(LINK.MY);
+	};
+
+	return (
+		<S.Background>
+			{modalState && (
+				<Modal
+					img="/assets/icons/modal-check.svg"
+					title="알림"
+					content={MESSAGE.PASSWORD.FIN}
+					onClose={handleCloseModal}
+				/>
+			)}
+
+			<S.Container>
+				<S.Header>
+					<S.Title>비밀번호 변경</S.Title>
+					<S.CloseBtn onClick={onClose} />
+				</S.Header>
+
+				<S.Body>
+					<S.InputBox>
+						<p>비밀번호</p>
+						<Input
+							variant="default"
+							size="height"
+							placeholder="현재 비밀번호"
+							name="password"
+							type="password"
+							onChange={handleChange}
+						/>
+						<Input
+							variant="default"
+							size="height"
+							placeholder="새 비밀번호"
+							name="newPassword"
+							type="password"
+							onChange={handleChange}
+						/>
+						<Input
+							variant="default"
+							size="height"
+							placeholder="새 비밀번호 확인"
+							name="newPwCheck"
+							type="password"
+							onChange={handleChange}
+						/>
+					</S.InputBox>
+
+					<S.Span>{match}</S.Span>
+				</S.Body>
+
+				<S.ButtonBox>
+					<Button variant="white" size="height" onClick={onClose}>
+						취소
+					</Button>
+					<Button variant="default" size="height" onClick={handleChangePw}>
+						확인
+					</Button>
+				</S.ButtonBox>
+			</S.Container>
+		</S.Background>
+	);
+}
+
+export default PwdModal;
---
+++ src/components/pages/MyPage/PwdModal/index.styles.js
@@ -0,0 +1,116 @@
+import styled, { css } from 'styled-components';
+import { ModalBackground, flexCenter, flexColumn } from 'styles/common';
+
+export const Background = styled.div`
+	${ModalBackground}
+	z-index: 101;
+`;
+
+export const Container = styled.div`
+	position: fixed;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	background-color: ${({ theme }) => theme.PALETTE.white[100]};
+	width: 294px;
+	padding: 32px 20px;
+	${flexColumn}
+	gap: 22px;
+
+	@media only screen and (min-width: 768px) {
+		width: 400px;
+		padding: 40px 40px;
+	}
+`;
+
+export const Header = styled.div`
+	${flexCenter}
+	justify-content: right;
+	gap: 56px;
+
+	@media only screen and (min-width: 768px) {
+		gap: 73px;
+	}
+`;
+
+export const Title = styled.p`
+	color: ${({ theme }) => theme.PALETTE.gray[300]};
+	font-size: ${({ theme }) => theme.FONT_SIZE.l};
+	font-weight: 500;
+
+	@media only screen and (min-width: 768px) {
+		font-size: ${({ theme }) => theme.FONT_SIZE.j};
+	}
+`;
+
+const buttonBgReset = css`
+	background: none;
+	width: 16px;
+	height: 16px;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: contain;
+
+	@media only screen and (min-width: 768px) {
+		width: 20px;
+		height: 20px;
+	}
+`;
+
+export const CloseBtn = styled.button`
+	${buttonBgReset}
+	background-image: url('/assets/icons/modal-x.svg');
+`;
+
+export const Body = styled.div`
+	${flexColumn}
+	gap: 3px;
+`;
+
+export const Span = styled.span`
+	font-size: ${({ theme }) => theme.FONT_SIZE.s};
+	color: ${({ theme }) => theme.PALETTE.orange[100]};
+	height: 10px;
+
+	@media only screen and (min-width: 768px) {
+		font-size: ${({ theme }) => theme.FONT_SIZE.ms};
+	}
+`;
+
+export const InputBox = styled.div`
+	${flexColumn}
+	gap: 8px;
+
+	> p {
+		font-size: ${({ theme }) => theme.FONT_SIZE.ms};
+		font-weight: 500;
+		color: ${({ theme }) => theme.PALETTE.orange[100]};
+	}
+
+	@media only screen and (min-width: 768px) {
+		> input {
+			height: 40px;
+		}
+
+		> p {
+			font-size: ${({ theme }) => theme.FONT_SIZE.l};
+		}
+	}
+`;
+
+export const ButtonBox = styled.div`
+	${flexCenter}
+	gap: 10px;
+
+	> button {
+		width: 66px;
+	}
+
+	@media only screen and (min-width: 768px) {
+		gap: 16px;
+
+		> button {
+			width: 130px;
+		}
+	}
+`;
