958df9341bb588e429a5a87bb98f9d5461178294
sunset <codinz.n@gmail.com>
2024-06-27 06:18:05 +0900

    Feat: 이용안내 데이터 가져오는 로직 추가

--- src/pages/Guide/index.js
+++ src/pages/Guide/index.js
@@ -1,3 +1,8 @@
+import { useEffect, useState } from 'react';
+
+import useFilter from 'hooks/useFilter';
+import useApi from 'hooks/useApi';
+
 import * as S from './index.styles';
 
 import Search from 'components/@common/Search';
@@ -6,14 +11,34 @@ import Filter from 'components/@common/Filter';
 import GuideList from 'components/pages/Guide/GuideList';
 
 function Guide() {
+	const { sort, handelSelectFilter } = useFilter();
+
+	const [data, setData] = useState([]);
+
+	const { result, trigger } = useApi({
+		path: `/client/guides?page=1&size=7&sortBy=${sort.sortBy}`,
+		shouldFetch: true,
+	});
+
+	useEffect(() => {
+		if (result.data) {
+			setData(result.data.guideList);
+		}
+
+		trigger({
+			path: `/client/guides?page=1&size=8&sortBy=${sort.sortBy}`,
+			applyResult: false,
+		});
+	}, [sort.sortBy, result.data]);
+
 	return (
 		<S.Body>
 			<Title title={'GUIDE'}>넷플레이스 이용안내</Title>
 
 			<S.MainBox>
 				<Search />
-				<Filter />
-				<GuideList />
+				<Filter onClick={handelSelectFilter} sort={sort} />
+				<GuideList list={data} />
 			</S.MainBox>
 
 			{/* 페이지네이션 */}
