ae4af3c5aeaea4aa224e540122da90512ae1467c
sunset <codinz.n@gmail.com>
2024-06-29 08:32:07 +0900

    Fix: inputBox props 추가
    ref 프룹스로 받기 위해서 수정

--- src/components/@common/InputBox/index.styles.js
+++ src/components/@common/InputBox/index.styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { flexAlignCenter } from 'styles/common';
 
 export const InputBox = styled.div`
@@ -46,4 +46,75 @@ export const Message = styled.p`
 	font-size: ${({ theme }) => theme.FONT_SIZE.ms};
 	color: ${({ theme }) => theme.PALETTE.red[100]};
 	white-space: pre-line;
+`;
+
+const sizeCSS = {
+	default: css`
+		width: 100%;
+		height: 40px;
+	`,
+	height: css`
+		width: 100%;
+		height: 28px;
+
+		@media screen and (min-width: 768px) {
+			height: 32px;
+		}
+
+		@media screen and (min-width: 1200px) {
+			height: 40px;
+		}
+	`,
+};
+
+export const Input = styled.input`
+	${({ size }) => sizeCSS[size]};
+
+	color: ${({ theme }) => theme.PALETTE.gray[200]};
+	border: 1px solid ${({ theme }) => theme.PALETTE.gray[0]};
+
+	&:focus {
+		border: 1px solid ${({ theme }) => theme.PALETTE.gray[100]};
+	}
+
+	font-size: ${({ theme }) => theme.FONT_SIZE.ms};
+	font-weight: 400;
+
+	padding: 0px 10px;
+	border-radius: 4px;
+
+	outline: none;
+
+	&::placeholder {
+		font-size: ${({ theme }) => theme.FONT_SIZE.ms};
+		color: ${({ theme }) => theme.PALETTE.gray[100]};
+		font-weight: 400;
+	}
+
+	/* 숫자 입력란의 증감키 숨기기 */
+	&[type='number']::-webkit-outer-spin-button,
+	&[type='number']::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+
+	&[type='number'] {
+		-moz-appearance: textfield; /* Firefox에서 증감키 숨기기 */
+	}
+
+	@media screen and (min-width: 768px) {
+		font-size: ${({ theme }) => theme.FONT_SIZE.m};
+
+		&::placeholder {
+			font-size: ${({ theme }) => theme.FONT_SIZE.m};
+		}
+	}
+
+	@media screen and (min-width: 1200px) {
+		font-size: ${({ theme }) => theme.FONT_SIZE.ml};
+
+		&::placeholder {
+			font-size: ${({ theme }) => theme.FONT_SIZE.ml};
+		}
+	}
 `;
