bb0c801a2c871c3e4b90167c1876018fb4a159d7
sunset <codinz.n@gmail.com>
2024-07-06 04:48:01 +0900

    Feat: 신청 상세 컴포넌트 신청 로직 추가
    - usePathname, useInput, useModal 훅 사용하여 경로, 입력 데이터 및 모달 상태 관리
    - Recoil을 사용하여 신청 데이터 상태 관리
    - 달력 열기/닫기 및 날짜 선택 기능 추가
    - 이전 버튼 클릭 시 상태 업데이트
    - 제출 시 유효성 검사 및 데이터 처리 로직 추가
    - 페이지 경로에 따라 다르게 렌더링되는 UI 요소 추가 (체험단, 홈페이지 제작 등)
    - 오류 메시지 상태 관리 및 표시 추가
    - 모달 컴포넌트 사용하여 알림 표시

--- src/components/pages/OtherTabs/ApplicationModal/ApplicationDetails/index.styles.js
+++ src/components/pages/OtherTabs/ApplicationModal/ApplicationDetails/index.styles.js
@@ -1,114 +1,15 @@
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
 import {
-	ModalBackground,
 	flexCenter,
 	flexColumn,
-	flexColumnCenter,
 	flexLeftCenter,
 	flexSpaceBetweenCenter,
 } from 'styles/common';
-
-export const Background = styled.div`
-	${ModalBackground}
-	z-index: 101;
-`;
-
-export const Container = styled.div`
-	position: fixed;
-	left: 50%;
-	top: 50%;
-	transform: translate(-50%, -50%);
-	background-color: ${({ theme }) => theme.PALETTE.white[100]};
-	width: 294px;
-	padding: 32px 24px;
-	${flexColumn}
-	gap: 20px;
-
-	@media only screen and (min-width: 768px) {
-		width: 568px;
-		padding: 46px 40px;
-	}
-
-	@media only screen and (min-width: 1200px) {
-		width: 1200px;
-		padding: 60px 102px;
-	}
-`;
-
-export const Header = styled.div`
-	${flexCenter}
-	position: relative;
-`;
-
-export const Title = styled.p`
-	color: ${({ theme }) => theme.PALETTE.gray[300]};
-	font-size: ${({ theme }) => theme.FONT_SIZE.ml};
-	font-weight: 600;
-	background-color: ${({ theme }) => theme.PALETTE.white[150]};
-	padding: 4px 8px;
-	border-radius: 8px;
-
-	@media only screen and (min-width: 768px) {
-		padding: 6px 10px;
-		font-size: ${({ theme }) => theme.FONT_SIZE.l};
-	}
-`;
-
-export const CloseBtn = styled.button`
-	background: none;
-	font-size: 0;
-	width: 16px;
-	height: 16px;
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: contain;
-	position: absolute;
-	right: 0;
-	background-image: url('/assets/icons/modal-x.svg');
-`;
-
-export const Step = styled.div`
-	${flexColumnCenter}
-	gap: 8px;
-	color: ${({ theme }) => theme.PALETTE.gray[300]};
-	font-size: ${({ theme }) => theme.FONT_SIZE.ms};
-
-	> img {
-		width: 90px;
-	}
-
-	> div {
-		${flexSpaceBetweenCenter}
-		gap: 27px;
-
-		> span {
-			color: ${({ theme }) => theme.PALETTE.gray[100]};
-		}
-
-		> p {
-			color: ${({ theme }) => theme.PALETTE.orange[100]};
-		}
-	}
-
-	@media screen and (min-width: 768px) {
-		font-size: ${({ theme }) => theme.FONT_SIZE.m};
-
-		> img {
-			width: 105px;
-		}
-	}
-
-	@media screen and (min-width: 768px) {
-		font-size: ${({ theme }) => theme.FONT_SIZE.ml};
-
-		> img {
-			width: 120px;
-		}
-	}
-`;
+import { DayPicker } from 'react-day-picker';
 
 export const Body = styled.div`
 	${flexColumn}
+	gap: 4px;
 `;
 
 export const InputBox = styled.div`
@@ -118,6 +19,72 @@ export const InputBox = styled.div`
 		font-weight: 400;
 		font-size: ${({ theme }) => theme.FONT_SIZE.ms};
 		color: ${({ theme }) => theme.PALETTE.red[100]};
+	}
+`;
+
+export const ExDate = styled(InputBox)`
+	width: 100%;
+
+	> div {
+		${flexCenter}
+		gap: 10px;
+		width: 100%;
+	}
+`;
+
+export const DateBox = styled.div`
+	position: relative;
+	width: 100%;
+`;
+
+export const CalendarImg = styled.img`
+	position: absolute;
+	width: 15px;
+	right: 5%;
+	top: 20%;
+	cursor: pointer;
+	content: ${({ $isOpen }) =>
+		$isOpen ? 'url("/assets/icons/calendar-hover.svg")' : 'none'};
+
+	&:hover {
+		content: url('/assets/icons/calendar-hover.svg');
+	}
+
+	@media screen and (min-width: 768px) {
+		width: 20px;
+		right: 5%;
+		top: 15%;
+	}
+`;
+
+export const Wrapper = styled.div`
+	background: ${({ theme }) => theme.PALETTE.white[100]};
+	${flexCenter}
+	border-radius: 6px;
+	position: absolute;
+	top: 120%;
+	left: -27%;
+	box-shadow: 0 0 4px 0 #00000029;
+
+	@media screen and (min-width: 768px) {
+		left: 0;
+	}
+
+	@media screen and (min-width: 1200px) {
+		font-size: ${({ theme }) => theme.FONT_SIZE.l};
+	}
+`;
+
+export const DayPickers = styled(DayPicker)`
+	.rdp {
+		--rdp-background-color: ${({ theme }) => theme.PALETTE.orange[50]};
+		--rdp-accent-color: ${({ theme }) => theme.PALETTE.orange[100]};
+		--rdp-accent-color-dark: ${({ theme }) => theme.PALETTE.orange[100]};
+	}
+
+	.rdp-day {
+		border-radius: 0;
+		text-align: center;
 	}
 `;
 
@@ -212,7 +179,7 @@ export const ButtonBox = styled.div`
 		}
 
 		> div > button {
-			width: 180px;
+			width: ${({ disabled }) => (disabled ? '376px' : '180px')};
 		}
 	}
 
@@ -220,11 +187,11 @@ export const ButtonBox = styled.div`
 		${flexSpaceBetweenCenter}
 		flex-direction: row;
 
-		> div {
-			width: auto;
+		& button {
+			width: 180px;
 		}
 
-		& button {
+		> div {
 			width: 180px;
 		}
 	}
