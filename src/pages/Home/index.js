b4fde0e6e5c88adfef1dcc8e70c68ae90bdc2c6d
sunset <codinz.n@gmail.com>
2024-07-06 05:10:33 +0900

    Refactor: home 코드 정리 및 css 수정

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,9 @@
 import * as S from './index.styles';
 
-import Line from 'components/@common/Line';
 import Title from 'components/@common/Title';
-import More from 'components/pages/Home/More';
 import List from 'components/pages/Home/List';
+import Line from 'components/@common/Line';
+import More from 'components/pages/Home/More';
 
 function Home() {
 	return (
