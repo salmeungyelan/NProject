d3a79f81c544caaa6112e6d1b24695386a453d93
sunset <codinz.n@gmail.com>
2024-07-08 06:52:41 +0900

    Fix: 페이지네이션 버튼 수정 및 로직 수정

--- src/components/@common/Pagination/index.styles.js
+++ src/components/@common/Pagination/index.styles.js
@@ -1,4 +1,4 @@
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
 import { flexCenter } from 'styles/common';
 
 export const PaginationBox = styled.div`
@@ -20,45 +20,75 @@ export const Number = styled.div`
 	cursor: pointer;
 	font-size: ${({ theme }) => theme.FONT_SIZE.m};
 
-	color: ${({ theme, $currentPage, $plus }) =>
-		$currentPage === $plus
-			? theme.PALETTE.white[100]
-			: theme.PALETTE.gray[200]};
+	color: ${({ theme, $selected }) =>
+		$selected ? theme.PALETTE.white[100] : theme.PALETTE.gray[200]};
 
 	border: 1px solid
-		${({ theme, $currentPage, $plus }) =>
-			$currentPage === $plus
-				? theme.PALETTE.orange[300]
-				: theme.PALETTE.gray[0]};
+		${({ theme, $selected }) =>
+			$selected ? theme.PALETTE.orange[300] : theme.PALETTE.gray[0]};
 
-	background-color: ${({ theme, $currentPage, $plus }) =>
-		$currentPage === $plus
-			? theme.PALETTE.orange[300]
-			: theme.PALETTE.white[100]};
+	background-color: ${({ theme, $selected }) =>
+		$selected ? theme.PALETTE.orange[300] : theme.PALETTE.white[100]};
+
+	&:hover {
+		color: ${({ theme, $selected }) => !$selected && theme.PALETTE.orange[100]};
+		border: 1px solid
+			${({ theme, $selected }) => !$selected && theme.PALETTE.orange[100]};
+	}
 `;
 
-export const HangulBtn = styled.div`
+export const HangulBtn = styled.button`
+	background-color: ${({ theme }) => theme.PALETTE.white[100]};
 	${flexCenter}
 	width: 48px;
 	height: 25px;
 	cursor: pointer;
 	border-radius: 4px;
 	font-size: ${({ theme }) => theme.FONT_SIZE.m};
-	color: ${({ theme }) => theme.PALETTE.gray[100]};
+	color: ${({ theme }) => theme.PALETTE.gray[200]};
 	border: 1px solid ${({ theme }) => theme.PALETTE.gray[0]};
 
 	&:hover {
+		color: ${({ theme }) => theme.PALETTE.orange[100]};
+		border: 1px solid ${({ theme }) => theme.PALETTE.orange[100]};
+	}
+
+	&:disabled {
+		cursor: not-allowed;
 		color: ${({ theme }) => theme.PALETTE.gray[100]};
-		border: 1px solid ${({ theme }) => theme.PALETTE.orange[300]};
+		border: 1px solid ${({ theme }) => theme.PALETTE.white[150]};
+		background-color: ${({ theme }) => theme.PALETTE.white[150]};
+
+		&:hover {
+			color: ${({ theme }) => theme.PALETTE.gray[100]};
+			border: 1px solid ${({ theme }) => theme.PALETTE.white[150]};
+		}
 	}
 `;
 
-export const ImgButton = styled(HangulBtn)`
+const buttonBgReset = css`
+	background: none;
+	font-size: 0;
 	width: 25px;
 	height: 25px;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: 10px 12px;
+`;
 
-	& img {
-		width: 10px;
-		height: 12px;
+export const LeftButton = styled(HangulBtn)`
+	${buttonBgReset}
+	background-image: url('/assets/icons/pagination-left.svg');
+
+	&:hover {
+		background-image: url('/assets/icons/pagination-left-color.svg');
+	}
+`;
+
+export const RightButton = styled(LeftButton)`
+	background-image: url('/assets/icons/pagination-right.svg');
+
+	&:hover {
+		background-image: url('/assets/icons/pagination-right-color.svg');
 	}
 `;
