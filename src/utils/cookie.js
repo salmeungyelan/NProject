dcae8c870555b43d599263fecb01cbc31d838576
sunset <codinz.n@gmail.com>
2024-07-19 14:20:16 +0900

    Refactor: 쿠키 처리 개선 및 js-cookie 라이브러리 도입
    - document.cookie 대신 js-cookie 라이브러리 사용
    - 쿠키의 만료 시간을 JWT 토큰의 `exp` 값에 따라 설정하도록 개선
    - accessToken과 refreshToken 쿠키의 만료 시간을 토큰의 유효기간에 맞게 자동 설정

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,37 +1,16 @@
+import Cookies from 'js-cookie';
+import decodeJWT from './token';
+
 const setCookie = (accessToken, refreshToken) => {
-	const oneDayInSeconds = 24 * 60 * 60; // 1일(24시간)의 초 단위 시간
-	const sevenDaysInSeconds = 7 * 24 * 60 * 60; // 7일(7 * 24시간)의 초 단위 시간
+	const { exp: access } = decodeJWT(accessToken);
+	let refresh = null;
+	if (refreshToken) ({ exp: refresh } = decodeJWT(refreshToken));
 
-	// accessToken 쿠키 설정 (1일 만료)
-	document.cookie = `accessToken=${accessToken}; max-age=${oneDayInSeconds};`;
+	const setExpires = exp => new Date(exp * 1000 + 9 * 60 * 60 * 1000);
 
-	// refreshToken 쿠키 설정 (7일 만료)
-	document.cookie = `refreshToken=${refreshToken}; max-age=${sevenDaysInSeconds};`;
+	Cookies.set('accessToken', accessToken, { expires: setExpires(access) });
+	if (refreshToken)
+		Cookies.set('refreshToken', refreshToken, { expires: setExpires(refresh) });
 };
 
-const getCookie = targetKey => {
-	const cookies = document.cookie.split('; ').map(cookie => {
-		const [key, value] = cookie.split('=');
-		return {
-			[key]: value,
-		};
-	});
-
-	const cookiesObject = Object.assign({}, ...cookies);
-
-	return cookiesObject[targetKey];
-};
-
-const deleteCookie = name => {
-	document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-};
-
-// 쿠키에서 토큰의 존재 여부를 확인하는 함수
-const checkToken = () => {
-	const tokenMatch = document.cookie.match(/(^|; )isToken=([^;]*)/);
-	const token = tokenMatch ? tokenMatch[2] : undefined;
-
-	return !!token;
-};
-
-export { setCookie, getCookie, deleteCookie, checkToken };
+export { setCookie };
