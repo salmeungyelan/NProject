ff804814a6606e1d845c206a8c7ced2a336074c5
sunset <codinz.n@gmail.com>
2024-07-25 18:17:23 +0900

    Refactor: 회원가입 페이지 seo 적용

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -11,6 +11,7 @@ import VALIDATE from 'utils/regex';
 
 import * as S from './index.styles';
 
+import Seo from 'components/@common/Seo/Seo';
 import Logo from 'components/@common/Logo';
 import InputBox from 'components/@common/InputBox';
 import Address from 'components/@common/Address';
@@ -309,162 +310,167 @@ function Register() {
 	};
 
 	return (
-		<S.Body>
-			{registerSuccess && (
-				<Modal
-					img="modal-check.svg"
-					title="알림"
-					content="회원가입이 완료되었습니다."
-					onClose={handleCloseSuccessModal}
-				/>
-			)}
+		<>
+			<Seo />
+			<S.Body>
+				{registerSuccess && (
+					<Modal
+						img="modal-check.svg"
+						title="알림"
+						content="회원가입이 완료되었습니다."
+						onClose={handleCloseSuccessModal}
+					/>
+				)}
 
-			<S.LogoBox>
-				<Link to={LINK.LOGIN}>
-					<Logo size="default" />
-				</Link>
-			</S.LogoBox>
+				<S.LogoBox>
+					<Link to={LINK.LOGIN}>
+						<Logo size="default" />
+					</Link>
+				</S.LogoBox>
 
-			<S.FormBox onSubmit={handleSubmit}>
-				<InputBox
-					title="이메일 입력"
-					name="email"
-					value={inputData.email}
-					placeholder="이메일을 입력해 주세요."
-					onChange={handleChange}
-					ref={emailRef}
-					message={errorMsg.email}
-					register
-				/>
+				<S.FormBox onSubmit={handleSubmit}>
+					<InputBox
+						title="이메일 입력"
+						name="email"
+						value={inputData.email}
+						placeholder="이메일을 입력해 주세요."
+						onChange={handleChange}
+						ref={emailRef}
+						message={errorMsg.email}
+						register
+					/>
 
-				<InputBox
-					title="아이디 입력"
-					name="username"
-					value={inputData.username}
-					placeholder="아이디는 영어와 숫자로만 입력해 주세요."
-					onChange={handleChange}
-					ref={idRef}
-					message={errorMsg.username}
-					register
-				/>
+					<InputBox
+						title="아이디 입력"
+						name="username"
+						value={inputData.username}
+						placeholder="아이디는 영어와 숫자로만 입력해 주세요."
+						onChange={handleChange}
+						ref={idRef}
+						message={errorMsg.username}
+						register
+					/>
 
-				<InputBox
-					title="비밀번호 입력"
-					name="password"
-					type="password"
-					value={inputData.password}
-					placeholder="비밀번호를 입력해 주세요."
-					onChange={handleChange}
-					ref={pwRef}
-					message={errorMsg.password}
-					register
-				/>
+					<InputBox
+						title="비밀번호 입력"
+						name="password"
+						type="password"
+						value={inputData.password}
+						placeholder="비밀번호를 입력해 주세요."
+						onChange={handleChange}
+						ref={pwRef}
+						message={errorMsg.password}
+						register
+					/>
 
-				<InputBox
-					title="비밀번호 확인"
-					type="password"
-					name="passwordCheck"
-					value={inputData.passwordCheck}
-					placeholder="비밀번호를 입력해 주세요."
-					onChange={handleChange}
-					ref={pwCheckRef}
-					message={errorMsg.passwordCheck}
-					register
-				/>
+					<InputBox
+						title="비밀번호 확인"
+						type="password"
+						name="passwordCheck"
+						value={inputData.passwordCheck}
+						placeholder="비밀번호를 입력해 주세요."
+						onChange={handleChange}
+						ref={pwCheckRef}
+						message={errorMsg.passwordCheck}
+						register
+					/>
 
-				<InputBox
-					title="업체명"
-					name="companyName"
-					value={inputData.companyName}
-					placeholder="업체명을 입력해 주세요."
-					onChange={handleChange}
-					ref={companyRef}
-					message={errorMsg.companyName}
-					register
-				/>
+					<InputBox
+						title="업체명"
+						name="companyName"
+						value={inputData.companyName}
+						placeholder="업체명을 입력해 주세요."
+						onChange={handleChange}
+						ref={companyRef}
+						message={errorMsg.companyName}
+						register
+					/>
 
-				<InputBox
-					title="전화번호"
-					name="contactNumber"
-					value={inputData.contactNumber}
-					placeholder="전화번호를 입력해 주세요."
-					onChange={handleChange}
-					ref={contactRef}
-					message={errorMsg.contactNumber}
-					register
-				/>
+					<InputBox
+						title="전화번호"
+						name="contactNumber"
+						value={inputData.contactNumber}
+						placeholder="전화번호를 입력해 주세요."
+						onChange={handleChange}
+						ref={contactRef}
+						message={errorMsg.contactNumber}
+						register
+					/>
 
-				<Address
-					message={errorMsg.address}
-					onChange={handleAddressChange}
-					ref={addressRef}
-					register
-					button
-				/>
+					<Address
+						message={errorMsg.address}
+						onChange={handleAddressChange}
+						ref={addressRef}
+						register
+						button
+					/>
 
-				<InputBox
-					title="사업자 등록 번호"
-					name="businessNumber"
-					value={inputData.businessNumber}
-					placeholder="업체의 사업자 등록 번호를 입력해 주세요."
-					onChange={handleChange}
-					ref={businessRef}
-					message={errorMsg.businessNumber}
-					register
-				/>
+					<InputBox
+						title="사업자 등록 번호"
+						name="businessNumber"
+						value={inputData.businessNumber}
+						placeholder="업체의 사업자 등록 번호를 입력해 주세요."
+						onChange={handleChange}
+						ref={businessRef}
+						message={errorMsg.businessNumber}
+						register
+					/>
 
-				<S.CheckBox>
-					<S.H1>약관동의</S.H1>
-					<S.Check>
-						<S.CheckAll>
-							<input
-								id="allCheck"
-								type="checkbox"
-								onChange={handleAllCheckedChange}
-								ref={termsRef}
-								checked={terms.allChecked}
-							/>
-							<label htmlFor="allCheck" />
-							<h1>전체동의</h1>
-						</S.CheckAll>
-						<Line size="width" variant="lightGray" />
-						<S.CheckItem>
-							<input
-								id="privacy"
-								type="checkbox"
-								onChange={() => handleIndividualChange('privacy')}
-								checked={terms.privacy}
-							/>
-							<label htmlFor="privacy" />
-							<p onClick={handleOpenModal(2)}>
-								개인정보 제공 및 활용에 동의합니다.
-							</p>
-							<span>(필수)</span>
-						</S.CheckItem>
-						<S.CheckItem>
-							<input
-								id="service"
-								type="checkbox"
-								onChange={() => handleIndividualChange('service')}
-								checked={terms.service}
-							/>
-							<label htmlFor="service" />
-							<p onClick={handleOpenModal(1)}>서비스 이용 약관에 동의합니다.</p>
-							<span>(필수)</span>
-						</S.CheckItem>
-					</S.Check>
-					<span>{errorMsg.terms || ' '}</span>
-				</S.CheckBox>
+					<S.CheckBox>
+						<S.H1>약관동의</S.H1>
+						<S.Check>
+							<S.CheckAll>
+								<input
+									id="allCheck"
+									type="checkbox"
+									onChange={handleAllCheckedChange}
+									ref={termsRef}
+									checked={terms.allChecked}
+								/>
+								<label htmlFor="allCheck" />
+								<h1>전체동의</h1>
+							</S.CheckAll>
+							<Line size="width" variant="lightGray" />
+							<S.CheckItem>
+								<input
+									id="privacy"
+									type="checkbox"
+									onChange={() => handleIndividualChange('privacy')}
+									checked={terms.privacy}
+								/>
+								<label htmlFor="privacy" />
+								<p onClick={handleOpenModal(2)}>
+									개인정보 제공 및 활용에 동의합니다.
+								</p>
+								<span>(필수)</span>
+							</S.CheckItem>
+							<S.CheckItem>
+								<input
+									id="service"
+									type="checkbox"
+									onChange={() => handleIndividualChange('service')}
+									checked={terms.service}
+								/>
+								<label htmlFor="service" />
+								<p onClick={handleOpenModal(1)}>
+									서비스 이용 약관에 동의합니다.
+								</p>
+								<span>(필수)</span>
+							</S.CheckItem>
+						</S.Check>
+						<span>{errorMsg.terms || ' '}</span>
+					</S.CheckBox>
 
-				{modalState && <TermsModal id={termsId} onClose={handleCloseModal} />}
+					{modalState && <TermsModal id={termsId} onClose={handleCloseModal} />}
 
-				<S.ButtonBox>
-					<Button size="default" variant="default" type="submit">
-						회원가입
-					</Button>
-				</S.ButtonBox>
-			</S.FormBox>
-		</S.Body>
+					<S.ButtonBox>
+						<Button size="default" variant="default" type="submit">
+							회원가입
+						</Button>
+					</S.ButtonBox>
+				</S.FormBox>
+			</S.Body>
+		</>
 	);
 }
 
